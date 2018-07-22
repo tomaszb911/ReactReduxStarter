664b6279d3f57ce209644d3bbc5fd3a10f272f9a
tburdka911 <burtek303@gmail.com>
2018-07-22 22:42:36 +0200

    YT api first part

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+const API_KEY = 'AIzaSyCc_9OIFYDG2CFzl-SgNIWYC_opJqqv2Rw';
 
-//Create a bew component.This component produce should
-//some HTML
-const App = function(){
-  return <div>Hi!</div>;
+
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+
+    });
+  }
+
+  render(){
+    return (
+    <div>
+      <SearchBar />
+      <VideoList videos={this.state.videos} />
+  </div>
+  );
+  }
 }
 
-//Take this generated component and put it
-//on the page (in the DOM)
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
